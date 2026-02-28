af4878cd74bf536e147edef1240d80c047f41eb7
Priyanka Srivastava <srivastavapriyanka71307@gmail.com>
2026-03-01 00:10:40 +0530

    trigger redeploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://skillswap-backend-5k4u.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
